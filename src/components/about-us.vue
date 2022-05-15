6e61cffdde2e2bdf1a79938760010a715e38c7d4
George <sanchez28112001@gmail.com>
2022-05-15 14:45:14 -0500

    added about section

---
+++ src/components/about-us.vue
@@ -0,0 +1,71 @@
+<template>
+  <section class="main_container">
+    <div class="left_box">
+      <div class="img_bg">
+        <div class="blue_filter">
+        </div>
+      </div>
+      <img src="" alt="">
+    </div>
+    <div class="right_box">
+      <h1 class="title_dr">Dr. Mesarina</h1>
+      <h3>¿Porqué acudir con nosotros?</h3>
+      <hr>
+      <p>Nuestro objetivo es curar ad íntegro la lesión que te aqueja, y en lesiones crónicas e irreversibles reducir el dolor, mejorar la movilidad y, sobre todo, la calidad de vida de nuestros pacientes.</p>
+      <v-btn style="background-color: #2397D7; width: fit-content" dark>Conocenos</v-btn>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "about-us"
+}
+</script>
+
+<style scoped>
+.main_container{
+  margin-top: 50px;
+  display: flex;
+  justify-content: space-evenly;
+  height: 50vh;
+}
+.img_bg {
+  background: url('../assets/about-img.png')no-repeat;
+  width: 80%;
+  height: 50vh;
+  background-size: cover;
+  background-position: center;
+
+}
+.left_box {
+  width: 50%;
+}
+.right_box{
+  width: 40%;
+  display: flex;
+  flex-direction: column;
+}
+.right_box * {
+  margin-bottom: 1rem;
+}
+.title_dr{
+  font-size: 3rem;
+  color: #054c73;
+}
+hr{
+  width: 20%;
+  background-color: #5b5b5b;
+  height: 3px;
+}
+@media (max-width: 695px){
+  .main_container {
+    height: 100vh;
+    flex-direction: column;
+    align-items: center;
+  }
+  .left_box, .right_box{
+    width: 90%;
+  }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="home">
     
+    <AboutUs></AboutUs>
     <Presentation></Presentation>
     <Contact style="margin: 100px 0 200px 0;"></Contact>
   </div>
@@ -10,13 +11,16 @@
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
 import Presentation from '@/components/service.vue'
+import Contact from "@/components/contact";
+import AboutUs from "@/components/about-us";
 
 export default {
   name: 'Home',
   components: {
     HelloWorld,
     Presentation,
-    Contact
+    Contact,
+    AboutUs
 }
 }
 </script>
