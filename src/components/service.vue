f6c8df323a9b99161e73c241284660b374f78cf8
Hugo Quispe Chavez <hugo.quispe12359@gmail.com>
2022-05-21 10:06:45 -0500

    cambios2

--- src/components/service.vue
+++ src/components/service.vue
@@ -3,22 +3,22 @@
             <v-container grid-list-md>
                 <v-layout row wrap>
                         <v-card width="200px" class="mx-auto" elevation="0" color="transparent">
-                            <v-img class="mx-auto" src="https://clinicasanjuandedioslima.pe/wp-content/uploads/2021/09/Recurso-8-2.png" width="77px" height="77px"></v-img>
+                            <v-img class="mx-auto" src="../assets/icon2.png" width="77px" height="77px"></v-img>
                             <v-card-title  class="justify-center" style="margin-top: 10px;font-size:80px; font-weight: medium; color:#024F78">100</v-card-title>
                             <v-card-subtitle class="d-flex justify-center" style="margin-top: 10px;font-size:20px; font-weight: medium; color:black">Pacientes Leales</v-card-subtitle>
                         </v-card>
                         <v-card width="200px" class="mx-auto"  elevation="0" color="transparent">
-                            <v-img class="mx-auto" src="https://clinicasanjuandedioslima.pe/wp-content/uploads/2021/09/Recurso-8-2.png" width="77px" height="77px"></v-img>
+                            <v-img class="mx-auto" src="../assets/icon1.png" width="77px" height="77px"></v-img>
                             <v-card-title  class="justify-center" style="margin-top: 10px;font-size:80px; font-weight: medium; color:#024F78">30</v-card-title>
                             <v-card-subtitle class="d-flex justify-center" style="margin-top: 10px;font-size:20px; font-weight: medium; color:black">Experiencia</v-card-subtitle>
                         </v-card>
                         <v-card width="200px" class="mx-auto" elevation="0" color="transparent">
-                            <v-img class="mx-auto" src="https://clinicasanjuandedioslima.pe/wp-content/uploads/2021/09/Recurso-8-2.png" width="77px" height="77px"></v-img>
+                            <v-img class="mx-auto" src="../assets/icon 2.png" width="77px" height="77px"></v-img>
                             <v-card-title  class="justify-center" style="margin-top: 10px;font-size:80px; font-weight: medium; color:#024F78">10</v-card-title>
                             <v-card-subtitle class="d-flex justify-center" style="margin-top: 10px;font-size:20px; font-weight: medium; color:black">Especialidades</v-card-subtitle>
                         </v-card>
                         <v-card width="200px" class="mx-auto" elevation="0" color="transparent">
-                            <v-img class="mx-auto" src="https://clinicasanjuandedioslima.pe/wp-content/uploads/2021/09/Recurso-8-2.png" width="77px" height="77px"></v-img>
+                            <v-img class="mx-auto" src="../assets/icon4.png" width="77px" height="77px"></v-img>
                             <v-card-title  class="justify-center" style="margin-top: 10px;font-size:80px; font-weight: medium; color:#024F78">40</v-card-title>
                             <v-card-subtitle class="d-flex justify-center" style="margin-top: 10px;font-size:20px; font-weight: medium; color:black">Tratamientos</v-card-subtitle>
                         </v-card>
