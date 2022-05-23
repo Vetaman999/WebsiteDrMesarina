96a1de484a1fe5d09d510b64bfd01988c0672b4a
Hugo Quispe Chavez <hugo.quispe12359@gmail.com>
2022-05-23 13:18:17 -0500

    b

--- src/components/card-profile.vue
+++ src/components/card-profile.vue
@@ -6,7 +6,7 @@
         <div>
             <div>
                 <h1 style="font-size: 59px; font-weight: bold; color: #2397D7;">Dr. Alejandro Mesarina</h1>
-                <h2 style="font-size: 20px; font-weight: lighter; color: #787575;">Médico doctorado en medicina y especialista en Cirugía Ortopédica</h2>
+                <h2 style="margin-top:-13px;font-size: 20px; font-weight: lighter; color: #787575;">Médico doctorado en medicina y especialista en Cirugía Ortopédica</h2>
             </div>
         </div>
     </div>
